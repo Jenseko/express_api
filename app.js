c36c4fcb3dedf4256bdd15719b2d86e06dc3fbae
Jenseko <jenscode23@gmail.com>
2023-05-17 13:15:36 +0200

    in progress

--- app.js
+++ app.js
@@ -15,24 +15,16 @@ app.get('/status', (req, res) => {
 // ----- LEVEL 2 -----------------------------
 
 
-app.get('/posts', async (req, res) => {
+app.get('/posts', (req, res) => {
 
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const json = await response.json();
-        console.log(json);
-
-        await fs.writeFile('./posts.json', JSON.stringify(json, null, 2))
-        const postFile = await fs.readFile('./post.json', 'utf-8');
-        const formattedData = JSON.parse(postFile);
-
-        res.send(formattedData);
-
-    } catch (error) {
-        console.log('Something went wrong!', error);
-        res.status(500).send('Internal Server Error');
-    }
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((data) => res.status(200).send(data))
+        .catch((error) => {
+            res.status(500).send('Internal Server Error');
+        });
 });
+
 
 app.listen(port, () => {
     console.log(`This app is running on port ${port}`);
